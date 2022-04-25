b5a955c94829c8ddcf81ea7b832328100ed90ecb
Nate Hathaway <natehathaway@byui.edu>
2022-04-25 12:11:42 -0600

    in class practice

---
+++ stringcase.py
@@ -0,0 +1,10 @@
+# In-class practice
+
+sentence = "The aWeSoMe TEACHER iS InstruCting the CLASS."
+
+print(sentence.lower())
+print(sentence.upper())
+print(sentence.capitalize())
+print(sentence)
+print(sentence.title())
+print(sentence.lower().count("t"))
