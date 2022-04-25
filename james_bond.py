f8d8e0bcce9f1311e035b276c2eb901425003561
Nate Hathaway <natehathaway@byui.edu>
2022-04-25 12:05:53 -0600

    trying out other formats

--- james_bond.py
+++ james_bond.py
@@ -1,6 +1,14 @@
+from cgi import print_arguments
+
+
 first_name = input ("What is your first name?")
 last_name = input ("What is your last name?")
 agent = input ("What is your code number?")
-" "
-print ("Your name is " + last_name + ", " + first_name + " " + last_name + ".")
+
+print(" ")
+#print("Your name is {}, {} {}.".format(last_name, first_name, last_name))
+#print(f"Your name is {last_name}, {first_name}, {last_name}.")
+print ("Your name is {1}, {0} {1}.".format(first_name, last_name))
+#print ("Your name is " + last_name + ", " + first_name + " " + last_name + ".")
+print (" ")
 print ("Welcome, Agent " + agent + ".")
