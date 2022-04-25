cb1c998f4a79495713f813867e675aeffec6a89a
Nate Hathaway <natehathaway@byui.edu>
2022-04-25 11:12:31 -0600

    Setting up repository

---
+++ favoritecolor.py
@@ -0,0 +1,12 @@
+from re import A
+from tokenize import Name
+from unicodedata import name
+
+
+A = input("What is your favorite color?")
+
+print ("The color you have chosen is......")
+print (" ")
+print (A)
+print (" ")
+print ("Congrats! You know what your favorite color is! Now you can tell all of your friends!")
---
+++ helloworld.py
@@ -0,0 +1,4 @@
+
+print("Hello World!")
+
+##THis is just a thing that sits here for my benefit
