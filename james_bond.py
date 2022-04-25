508e7687e73cc30b04f499845845774d916a96b6
Nate Hathaway <natehathaway@byui.edu>
2022-04-25 11:42:50 -0600

    making repository

---
+++ james_bond.py
@@ -0,0 +1,6 @@
+first_name = input ("What is your first name?")
+last_name = input ("What is your last name?")
+agent = input ("What is your code number?")
+" "
+print ("Your name is " + last_name + ", " + first_name + " " + last_name + ".")
+print ("Welcome, Agent " + agent + ".")
