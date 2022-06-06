951628ed249410bd9fb7bd10f61adcede7edb2b2
Nate Hathaway <natehathaway@byui.edu>
2022-06-06 10:23:26 -0600

    class

---
+++ for_loops.py
@@ -0,0 +1,25 @@
+
+
+#color list
+
+colors = ["red", "blue", "green", "yellow"]
+
+for color in colors:
+    print(color)
+
+print()
+
+#Numbers 1-8
+
+for i in range(1, 9):
+    print(i)
+
+
+print()
+#numbers 2-20 count by 2s
+
+for i in range(2, 21, 2):
+    print(i)
+
+
+
