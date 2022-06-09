31692af6afab5317bccf9e088207db4fb8fcaa8f
Nate Hathaway <natehathaway@byui.edu>
2022-06-08 21:00:52 -0600

    homework

--- word_game.py
+++ word_game.py
@@ -2,31 +2,48 @@
 
 #Welcome to the word guessing game!
 
-#What is your guess? temple
-#Your guess was not correct.
-#What is your guess? moroni
-#Your guess was not correct.
-#What is your guess? mosiah
-#Congratulations! You guessed it!
-#It took you 3 guesses.
 
+from ast import While
 from itertools import count
 
 
+#variables
 secret_word = "nuggets"
+word = []
+play = True
 
+#Welcome and hint
 print("Welcome to the word guessing game!")
 
-word = input("What is your guess? ")
+
+# Fills in word with _ according to length of secret_word.
+for letter in secret_word:
+    print(" _", end="")
+    word.append(" _")
+print()
 count = 0
-while word != secret_word:
-    print("Your guess was not correct.")
-    word = input("What is your guess? ")
-    count += 1
-
-if word == secret_word:
-    print("Congratulations! You guessed it!")
-    print("It took you", count, "guesses.")
 
 
-print("press ENTER to exit")
+#play loop will continue until user types "quit"
+while play:
+    guess = input("What is your guess? ").lower()
+    if guess == "quit":
+        break
+
+    #Checks guess word against secret word
+    for i in range(len(guess)):
+        #if lettter is correct and correct place
+        if guess[i] == secret_word[i]:
+            word[i] = secret_word[i].upper()
+        #If letter is correct but in wrong place
+        elif guess[i] in secret_word:
+            word[i] = guess[i].lower()
+    #Prints the word in it's current state.
+    for letter in word:
+        print(letter, end="")
+    print()
+    # Prints congratulations message after win condition.
+    if word == [letter for letter in secret_word.upper()]:
+        print("Congratulations, you guessed the word!")
+        break
+print("Thanks for playing!")
